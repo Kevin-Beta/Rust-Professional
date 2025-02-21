6f52fefd862d8b73fcdef8233055e36d94e9f4b5
Kevin-Beta <52095338+Kevin-Beta@users.noreply.github.com>
2025-02-21 11:38:44 +0800

    solve easy08

--- exercises/easy/algorithm8.rs
+++ exercises/easy/algorithm8.rs
@@ -1,6 +1,6 @@
 /*
 	queue
-	This question requires you to use queues to implement the functionality of the stac
+	This question requires you to use queues to implement the functionality of the stack
 */
 
 
@@ -55,27 +55,40 @@ impl<T> Default for Queue<T> {
 pub struct myStack<T>
 {
 	//TODO
-	q1:Queue<T>,
-	q2:Queue<T>
+	q1: Queue<T>,
+	q2: Queue<T>
 }
 impl<T> myStack<T> {
     pub fn new() -> Self {
         Self {
 			//TODO
-			q1:Queue::<T>::new(),
-			q2:Queue::<T>::new()
+			q1: Queue::<T>::new(),
+			q2: Queue::<T>::new()
         }
     }
     pub fn push(&mut self, elem: T) {
         //TODO
+        while let Ok(t) = self.q1.dequeue() {
+            self.q2.enqueue(t)
+        }
+        self.q1.enqueue(elem);
     }
     pub fn pop(&mut self) -> Result<T, &str> {
         //TODO
-		Err("Stack is empty")
+        if let Ok(t) = self.q1.dequeue() {
+            std::mem::swap(&mut self.q1, &mut self.q2);
+            while self.q1.size() > 1 {
+                self.q2.enqueue( self.q1.dequeue().unwrap() );
+            }
+            Ok(t)
+        }else{
+            Err("Stack is empty")
+        }
+
     }
     pub fn is_empty(&self) -> bool {
 		//TODO
-        true
+        self.q1.size() == 0
     }
 }
 
